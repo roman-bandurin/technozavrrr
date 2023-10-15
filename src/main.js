73840f7706a4a6ba67fa3f8a46bf01658456b272
Роман Бандурин <roman.obj@mail.ru>
2023-10-15 04:14:28 +0300

    added store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue"
 import App from "./App.vue"
+import store from "./store"
 import router from "./router"
 
 import { game1, game2 } from "./constants"
@@ -11,6 +12,7 @@ import out from "./utils"
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: (h) => h(App),
 }).$mount("#app")
